f800f4de584c048ec600e577be2150828deb204a
Leandro C <leandro.ca.98@gmail.com>
2024-12-18 11:22:52 -0300

    habilities section

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss'
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit, OnDestroy {
+  genderToggle: boolean = true; // True para "o", False para "a"
+  private intervalId!: any;
+
+  ngOnInit(): void {
+    if (typeof window !== 'undefined') {
+      this.intervalId = setInterval(() => {
+        this.genderToggle = !this.genderToggle;
+      }, 2500);
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
 }
